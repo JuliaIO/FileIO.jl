61b4f4f79895b174c447743b25b4159d598902d6
brk00 <p.oliveira.castro@gmail.com>
2015-04-20 20:15:41 -0300

    corrected typo

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,4 +8,4 @@ test_file = File("test.txt")
 @test abspath(test_file) === test_file.abspath
 @test ending(test_file)  === :txt
 
-@test_throw file"inexistent_file.txt"
+@test_throws file"inexistent_file.txt"
