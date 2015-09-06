60be9af53abf42e2b801fb2fe0e1e7f1ea8b1a64
Simon Danisch <sdanisch@gmail.com>
2015-09-07 00:18:12 +0200

    re-arrange file formats

--- src/registry.jl
+++ src/registry.jl
@@ -11,49 +11,12 @@ add_format(format"PBMBinary", b"P4", ".pbm")
 add_format(format"PGMBinary", b"P5", ".pgm")
 add_format(format"PPMBinary", b"P6", ".ppm")
 
-
-### Complex cases
-# HDF5: the complication is that the magic bytes may start at
-# 0, 512, 1024, 2048, or any multiple of 2 thereafter
-h5magic = (0x89,0x48,0x44,0x46,0x0d,0x0a,0x1a,0x0a)
-function detecthdf5(io)
-    position(io) == 0 || return false
-    seekend(io)
-    len = position(io)
-    seekstart(io)
-    magic = Array(UInt8, length(h5magic))
-    pos = position(io)
-    while pos+length(h5magic) <= len
-        read!(io, magic)
-        if iter_eq(magic, h5magic)
-            return true
-        end
-        pos = pos == 0 ? 512 : 2*pos
-        if pos < len
-            seek(io, pos)
-        end
-    end
-    false
-end
-add_format(format"HDF5", detecthdf5, [".h5", ".hdf5"])
-add_loader(format"HDF5", :HDF5)
-add_saver(format"HDF5", :HDF5)
-
-
-
-add_format(format"GLSLShader", (), [".frag", ".vert", ".geom", ".comp"])
-add_loader(format"GLSLShader", :GLAbstraction)
-add_saver(format"GLSLShader", :GLAbstraction)
-
-
 add_format(format"NRRD", "NRRD", [".nrrd", ".nhdr"])
 add_loader(format"NRRD", :NRRD)
 add_saver(format"NRRD", :NRRD)
 
-
 add_format(format"AndorSIF", "Andor Technology Multi-Channel File", ".sif")
 add_loader(format"AndorSIF", :AndorSIF)
-
 
 add_format(format"BMP", UInt8[0x42,0x4d], ".bmp")
 add_loader(format"BMP", :ImageMagick)
@@ -125,9 +88,12 @@ add_format(format"WPG", UInt8[0xff,0x57,0x50,0x43], ".wpg")
 add_loader(format"WPG", :ImageMagick)
 add_saver(format"WPG", :ImageMagick)
 
+#Shader files
+add_format(format"GLSLShader", (), [".frag", ".vert", ".geom", ".comp"])
+add_loader(format"GLSLShader", :GLAbstraction)
+add_saver(format"GLSLShader", :GLAbstraction)
 
 # Mesh formats
-
 add_format(format"OBJ", (), ".obj")
 add_loader(format"OBJ", :MeshIO)
 add_saver(format"OBJ", :MeshIO)
@@ -144,10 +110,40 @@ add_format(format"2DM", "MESH2D", ".2dm")
 add_loader(format"2DM", :MeshIO)
 add_saver(format"2DM", :MeshIO)
 
-
 add_format(format"OFF", "OFF", ".off")
 add_loader(format"OFF", :MeshIO)
 add_saver(format"OFF", :MeshIO)
+
+
+
+
+### Complex cases
+# HDF5: the complication is that the magic bytes may start at
+# 0, 512, 1024, 2048, or any multiple of 2 thereafter
+h5magic = (0x89,0x48,0x44,0x46,0x0d,0x0a,0x1a,0x0a)
+function detecthdf5(io)
+    position(io) == 0 || return false
+    seekend(io)
+    len = position(io)
+    seekstart(io)
+    magic = Array(UInt8, length(h5magic))
+    pos = position(io)
+    while pos+length(h5magic) <= len
+        read!(io, magic)
+        if iter_eq(magic, h5magic)
+            return true
+        end
+        pos = pos == 0 ? 512 : 2*pos
+        if pos < len
+            seek(io, pos)
+        end
+    end
+    false
+end
+add_format(format"HDF5", detecthdf5, [".h5", ".hdf5"])
+add_loader(format"HDF5", :HDF5)
+add_saver(format"HDF5", :HDF5)
+
 
 function detect_stlascii(io)
     position(io) != 0 && return false
