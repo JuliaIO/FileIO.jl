9829adf19f2511242af7d6c19ff7c5e601227bad
Sdanisch <sdanisch@gmail.com>
2015-09-18 19:39:54 +0200

    fixed very silly error

--- src/registry.jl
+++ src/registry.jl
@@ -177,7 +177,9 @@ function detect_stlbinary(io)
     skip(io, number_of_triangle_blocks*size_triangleblock-sizeof(Uint16))
     attrib_byte_count = read(io, Uint16) # read last attrib_byte
     attrib_byte_count != zero(Uint16) && (seekstart(io); return false) # should be zero as not used
-    (seekstart(io); return eof(io)) # if end of file, we have a stl!
+    result = eof(io) # if end of file, we have a stl!
+    seekstart(io)
+    return result
 end
 add_format(format"STL_ASCII", detect_stlascii, [".stl", ".STL"])
 add_format(format"STL_BINARY", detect_stlbinary, [".stl", ".STL"])
