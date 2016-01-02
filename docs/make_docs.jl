fef9817ec1ed90ad00ba3f88c1400455b9a8e6e8
SimonDanisch <sdanisch@gmail.com>
2016-01-02 21:28:50 +0100

    include link in readme

--- docs/make_docs.jl
+++ docs/make_docs.jl
@@ -1,10 +1,23 @@
 using FileIO, Compat
 import FileIO: LOAD, SAVE, OSX, OS
 const fs = open(Pkg.dir("FileIO", "docs", "registry.md"), "w")
+
+function pkg_url(pkgname)
+    result = readchomp(Pkg.dir("METADATA", string(pkgname), "url"))
+    g = "git://"
+    if startswith(result, g)
+        return string("http://", result[length(g):end])
+    end
+    result
+end
+library2string(x) = "[$(x)]($(pkg_url(x)))"
+
 extension2string(x) = join(map(string, x), ", ")
 extension2string(x::AbstractString) = x
-os2string(x::Vector) = isempty(x) ? "ALL" : join(map(os2string, x), ", ")
-os2string{O <: OS}(os::Type{O}) = string(O.name.name)
+
+os2string(x::Vector) = isempty(x) ? "**all** platforms " : join(map(os2string, x), ", ")
+os2string{O <: OS}(os::Type{O}) = "**$(O.name.name)**"
+
 magic2string(x::Function) = "has detection function"
 magic2string(x::Tuple) = isempty(x) ? "only extension": string(x)
 magic2string(x) = string(x)
@@ -16,11 +29,11 @@ function loadsave2string(load_save_libraries)
 
         os, loadsave = FileIO.split_predicates(predicates)
         if isempty(loadsave)
-            print(io, "loads and saves on all platforms with ", library)
+            print(io, "loads and saves on **all** platforms with ", library2string(library))
         elseif (LOAD in loadsave)
-            print(io, "loads with ", library, " on: ", os2string(os), " ")
+            print(io, "loads with ", library2string(library), " on: ", os2string(os), " ")
         elseif (SAVE in loadsave)
-            print(io, "loads with ", library, " on: ", os2string(os), " ")
+            print(io, "loads with ", library2string(library), " on: ", os2string(os), " ")
         end
     end
     takebuf_string(io)
