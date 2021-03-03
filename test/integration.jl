79a49d45638d02f364494a351f842969e4c9af00
Tim Holy <tim.holy@gmail.com>
2021-03-03 13:20:42 -0600

    Add support for URIs (#302)
    User must load HTTP manually for this to be activated.
    (It exploits Requires.)
    
    Closes #219
    Closes #89

--- test/integration.jl
+++ test/integration.jl
@@ -1,11 +1,12 @@
 using FileIO
 using ColorTypes
 using ColorTypes.FixedPointNumbers
+using HTTP
 using Pkg
 using Test
 
 @testset "Integration" begin
-    if haskey(ENV, "CI") && Base.VERSION >= v"1.3"
+    if haskey(ENV, "CI") && Base.VERSION >= v"1.3"  # ImageIO can't be loaded on Julia < 1.3, so putting it as test dep is problematic
         Pkg.add("ImageIO")
         img = rand(RGB{N0f8}, 50, 50)
         io = IOBuffer()
@@ -14,5 +15,8 @@ using Test
         io2 = IOBuffer(buf)
         img2 = load(io2)
         @test img2 == img
+
+        uri = HTTP.URI("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Wikipedia-logo-v2-en.svg/135px-Wikipedia-logo-v2-en.svg.png")
+        @test isa(load(uri), Matrix)
     end
 end
