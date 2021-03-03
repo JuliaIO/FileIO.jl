e7d7672985bd1d73533054831e96e24fce23fa02
Tim Holy <tim.holy@gmail.com>
2021-03-03 11:30:04 -0600

    Allow integration test to work on julia < 1.3

--- test/integration.jl
+++ test/integration.jl
@@ -1,14 +1,18 @@
 using FileIO
 using ColorTypes
 using ColorTypes.FixedPointNumbers
+using Pkg
 using Test
 
 @testset "Integration" begin
-    img = rand(RGB{N0f8}, 50, 50)
-    io = IOBuffer()
-    save(Stream{format"PNG"}(io), img)
-    buf = take!(io)
-    io2 = IOBuffer(buf)
-    img2 = load(io2)
-    @test img2 == img
+    if haskey(ENV, "CI")
+        Pkg.add(Base.VERSION >= v"1.3" ? "ImageIO" : "ImageMagick")
+        img = rand(RGB{N0f8}, 50, 50)
+        io = IOBuffer()
+        save(Stream{format"PNG"}(io), img)
+        buf = take!(io)
+        io2 = IOBuffer(buf)
+        img2 = load(io2)
+        @test img2 == img
+    end
 end
