c201e5cb219a8021bc2e81e199b7be31937cc681
Abhro R <5664668+abhro@users.noreply.github.com>
2024-07-11 15:10:37 -0400

    Add Downloads to test dependencies
    Base.download() is deprecated

--- test/query.jl
+++ test/query.jl
@@ -1,6 +1,7 @@
 using FileIO
 using Test
 using Random
+import Downloads
 
 @testset "OS" begin
     if Sys.islinux()
@@ -399,19 +400,19 @@ let file_dir = joinpath(@__DIR__, "files"), file_path = Path(file_dir)
             @test typeof(q) <: File{format"AVI"}
         end
         @testset "MP4 detection" begin
-            f = download("https://archive.org/download/LadybirdOpeningWingsCCBYNatureClip/Ladybird%20opening%20wings%20CC-BY%20NatureClip.mp4")
+            f = Downloads.download("https://archive.org/download/LadybirdOpeningWingsCCBYNatureClip/Ladybird%20opening%20wings%20CC-BY%20NatureClip.mp4")
             q = query(f)
             @test typeof(q) <: File{format"MP4"}
         end
         if Base.VERSION >= v"1.6" || !Sys.iswindows()
             # FIXME: Windows fails to download the files on Julia 1.0
             @testset "OGG detection" begin
-                f = download("https://upload.wikimedia.org/wikipedia/commons/8/87/Annie_Oakley_shooting_glass_balls%2C_1894.ogv")
+                f = Downloads.download("https://upload.wikimedia.org/wikipedia/commons/8/87/Annie_Oakley_shooting_glass_balls%2C_1894.ogv")
                 q = query(f)
                 @test typeof(q) <: File{format"OGG"}
             end
             @testset "MATROSKA detection" begin
-                f = download("https://upload.wikimedia.org/wikipedia/commons/1/13/Artist%E2%80%99s_impression_of_the_black_hole_inside_NGC_300_X-1_%28ESO_1004c%29.webm")
+                f = Downloads.download("https://upload.wikimedia.org/wikipedia/commons/1/13/Artist%E2%80%99s_impression_of_the_black_hole_inside_NGC_300_X-1_%28ESO_1004c%29.webm")
                 q = query(f)
                 @test typeof(q) <: File{format"MATROSKA"}
             end
